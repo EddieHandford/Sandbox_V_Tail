4df334e81ef44cfe9c21546b6503b44e31137c1e
EddieHandford <47256888+EddieHandford@users.noreply.github.com>
2025-10-24 20:05:37 +0100

    Testing Compass Function
    Seems to work, but slowly

--- Sandbox_RC_Glider_Software/V_Tail_Micro_Bit-main.py
+++ Sandbox_RC_Glider_Software/V_Tail_Micro_Bit-main.py
@@ -1,9 +1,24 @@
+#To Do List
+#Take ELRS RC input 
+#Mix the Vtail outputs pitch and yaw axis and 
+#include option to reverse the servo
+#Test system can be manually flown
+#Introduce a compass hold mode - ideally toggleable with an RC channel
+
 # Imports go at the top
 from microbit import *
 
 
+#Data_Dictionary
+Data_Compass = 0.0
+
+
+
 # Code in a 'while True:' loop repeats forever
 while True:
-    display.show(Image.HEART)
-    sleep(1000)
-    display.scroll('Hello')
+    
+    Data_Compass = compass.heading()
+    
+    display.scroll(Data_Compass)
+    
+    print(Data_Compass)
